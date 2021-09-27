298db4232db5ca19891d0b0d99c752bc817354d2
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-09-27 15:36:23 -0500

    trying to get search working

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,6 +10,14 @@ namespace TechJobsMVC.Controllers
 {
     public class SearchController : Controller
     {
+        internal static Dictionary<string, string> ColumnChoices = new Dictionary<string, string>()
+        {
+            {"all", "All"},
+            {"employer", "Employer"},
+            {"location", "Location"},
+            {"positionType", "Position Type"},
+            {"coreCompetency", "Skill"}
+        };
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -14,5 +26,26 @@ namespace TechJobsMVC.Controllers
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.ColumnChoices;
+            List<Job> jobs = new List<Job>();
+            try
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm = "all");
+            }
+            catch (NullReferenceException e)
+            {
+                if (e.Source.Equals("null"))
+                {
+                    jobs = JobData.FindAll();
+
+                }
+            }
+
+            ViewBag.jobs = jobs;
+            
+            return View();
+        }
     }
 }

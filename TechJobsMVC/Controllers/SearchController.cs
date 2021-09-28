fea4ba329cdad15dcf9a53d10dd0d95cc290175c
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-09-27 20:19:18 -0500

    finished

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -10,42 +10,54 @@ namespace TechJobsMVC.Controllers
 {
     public class SearchController : Controller
     {
-        internal static Dictionary<string, string> ColumnChoices = new Dictionary<string, string>()
-        {
-            {"all", "All"},
-            {"employer", "Employer"},
-            {"location", "Location"},
-            {"positionType", "Position Type"},
-            {"coreCompetency", "Skill"}
-        };
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
             ViewBag.columns = ListController.ColumnChoices;
+            //ViewBag.tableChoices = ListController.TableChoices;
             return View();
         }
 
+        [HttpPost]
         // TODO #3: Create an action method to process a search request and render the updated search view. 
         public IActionResult Results(string searchType, string searchTerm)
         {
             ViewBag.columns = ListController.ColumnChoices;
+            ViewBag.tableChoices = ListController.TableChoices;
             List<Job> jobs = new List<Job>();
-            try
-            {
-                jobs = JobData.FindByColumnAndValue(searchType, searchTerm = "all");
-            }
-            catch (NullReferenceException e)
-            {
-                if (e.Source.Equals("null"))
-                {
-                    jobs = JobData.FindAll();
 
-                }
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                jobs = JobData.FindAll();
+            }
+            else 
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
             }
 
+            ViewBag.Title = "Jobs";
             ViewBag.jobs = jobs;
             
-            return View();
+            return View("Index");
         }
+       
+        /*public IActionResult Results(string searchType, string searchTerm = "all")
+        {
+            List<Job> jobs = new List<Job>();
+            if (searchTerm.ToLower().Equals("all"))
+            {
+                jobs = JobData.FindAll();
+                
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "Jobs with " + ColumnChoices[searchType] + ": " + searchTerm;
+            }
+            ViewBag.jobs = jobs;
+
+            return View();
+        }*/
     }
 }
